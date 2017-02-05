7fdc681855064c06433bd94f88b8a4bdc8b3732b
irom77 <irek.romaniuk@gmail.com>
2017-02-05 12:17:02 -0500

    adding src and dst Zones

--- main.go
+++ main.go
@@ -89,8 +89,9 @@ func main () {
 	}
 	var Threats []Threat
 	for _, row := range res[0].Series[0].Values {
-		Threats = append(Threats,Threat{row[1].(string), row[10].(string), row[2].(string),row[11].(string), row[3].(string),row[4].(string),
-						row[5].(string), row[6].(string), row[7].(string), row[8].(string)})
+		fmt.Println(row)
+		Threats = append(Threats,Threat{row[1].(string), row[2].(string), row[3].(string),row[4].(string), row[5].(string),row[6].(string),
+						row[7].(string), row[8].(string), row[9].(string), row[10].(string)})
 	}
 	buf := new(bytes.Buffer)
 	th := template.Must(template.New("html table").Parse(tmplhtml))
